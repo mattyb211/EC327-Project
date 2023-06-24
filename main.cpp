bfb96e387deeb7c67f6364dba0946f752e14287f
carsonmo48 <136993429+carsonmo48@users.noreply.github.com>
2023-06-24 12:31:15 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
+
+// -I/usr/local/Cellar/sfml/2.5.1_2/include -L/usr/local/Cellar/sfml/2.5.1_2/lib -lsfml-graphics -lsfml-system -lsfml-window -lsfml-audio -lsfml-window
+
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
     sf::CircleShape shape(100.f);
+    sf::Vector2f circlePos(500,500);
+    shape.setPosition(circlePos);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
@@ -14,6 +19,39 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        if (circlePos.x >= 0) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+                shape.move(sf::Vector2f(-0.25,0));
+                circlePos.x -= 0.25;
+            } 
+            shape.setPosition(circlePos);
+        }
+
+        if (circlePos.x <=800) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+                shape.move(sf::Vector2f(0.25,0));
+                circlePos.x += 0.25;
+            } 
+            shape.setPosition(circlePos);
+        }
+        
+        if (circlePos.y >= 0) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
+                shape.move(sf::Vector2f(0,-0.25));
+                circlePos.y -= 0.25;
+            } 
+            shape.setPosition(circlePos);
+        }
+        
+        if (circlePos.y <= 800) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
+                shape.move(sf::Vector2f(0,0.25));
+                circlePos.y += 0.25;
+            }
+            shape.setPosition(circlePos);
+        }
+        
 
         window.clear();
         window.draw(shape);
