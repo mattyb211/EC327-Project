d9a1db764bf9426836d093ef31d9ba0922c6a40f
Mateo Balzola <mbalzola@bu.edu>
2023-06-25 16:03:15 -0400

    -a

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 
-int main($ git pull)
+int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
     sf::CircleShape shape(100.f);
@@ -19,6 +19,20 @@ int main($ git pull)
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        if (!countdown.isFinished()) {
+        // Update countdown text and draw it
+        int remainingSeconds = static_cast<int>(countdown.getRemainingTime().asSeconds());
+        countdownText.setString(std::to_string(remainingSeconds));
+        window.draw(countdownText);
+    } else {
+        // Game logic goes here
+        // ...
+        Scoreboard.Player1Score++;
+        Scoreboard.Player2Score++;
+        Scoreboard.update();
+        Scoreboard.draw(window);
+    }
 
         if (circlePos.x >= 0) {
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
@@ -60,3 +74,79 @@ int main($ git pull)
 
     return 0;
 }
+
+class Timer {
+    sf::Clock clock;
+    sf::Time targetTime;
+    bool finished = false;
+public:
+    Timer(const sf::Time& countdown_time) : targetTime(countdown_time) {}
+
+    void reset() {
+        clock.restart();
+        finished = false;
+    }
+
+    bool isFinished() {
+        if (!finished) {
+            if (clock.getElapsedTime() >= targetTime) {
+                finished = true;
+            }
+        }
+        return finished;
+    }
+
+    sf::Time getRemainingTime() {
+        if (finished) {
+            return sf::Time::Zero;
+        }
+        sf::Time remaining = targetTime - clock.getElapsedTime();
+        return remaining;
+    }
+};
+
+
+Timer countdown(sf::seconds(3)); // 3 second countdown
+sf::Text countdownText;
+countdownText.setFont(font); // Assuming you have a loaded sf::Font named font
+countdownText.setCharacterSize(32);
+countdownText.setFillColor(sf::Color::White);
+countdownText.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+
+struct Scoreboard {
+    int Player1Score = 0;
+    int Player2Score = 0;
+
+    sf::Font font;
+    sf::Text Player1Text;
+    sf::Text Player2Text;
+
+    Scoreboard() {
+        if (!font.loadFromFile("Arial.ttf")) {
+            std::cerr << "Failed to load font" << std::endl;
+            return;
+        }
+
+        Player1Text.setFont(font);
+        Player1Text.setCharacterSize(24);
+        Player1Text.setFillColor(sf::Color::Red);
+        Player1Text.setPosition(10, 10);
+
+        Player2Text.setFont(font);
+        Player2Text.setCharacterSize(24);
+        Player2Text.setFillColor(sf::Color::Blue);
+        Player2Text.setPosition(10, 50);
+
+        update();
+    }
+
+    void update() {
+        Player1Text.setString("Player1: " + std::to_string(Player1Score));
+        Player2Text.setString("Player2: " + std::to_string(Player2Score));
+    }
+
+    void draw(sf::RenderWindow &window) {
+        window.draw(Player1Text);
+        window.draw(Player2Text);
+    }
+};
